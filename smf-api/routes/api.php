93fbd798711c45a2e41b126516eebe74853bb29e
nattharee <penquin501@gmail.com>
2025-06-30 16:04:34 +0700

    first commit

---
+++ smf-api/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
